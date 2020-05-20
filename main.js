471e953da7bb464c476e42e9d228aae023876e9c
N. Monalisa <nathaliamonalisa@gmail.com>
2020-05-19 22:33:30 -0300

    Updates

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-import { example } from './data.js';
-import data from './data/pokemon/pokemon.js';
-import pokemon from './data/pokemon/pokemon.js';
-console.log(data)
-
-
+import { filterData, sortData } from "./data.js";
+import data from "./data/pokemon/pokemon.js";
 
 //Variáveis globais: configuração
 const colorTypeList = {
     Bug: "#1E6DE3",
+    Dragon: "#ff6347",
     Electric: "#D7DB1E",
     Fighting: "#FEC807",
     Fire: "#FF8C00",
@@ -26,199 +23,206 @@ const colorTypeList = {
 
 // ---------------- Estruturação dos cards ------------------------//
 
-const cloneCards = () => document.querySelector('.container-deck').appendChild(document.querySelector('.container-card').cloneNode(true));
+const cloneCards = () =>
+    document
+    .querySelector(".container-deck")
+    .appendChild(document.querySelector(".container-card").cloneNode(true));
 
 const setInfosOnCard = (node, index, data, attribute) => {
-    let parentNode = document.querySelectorAll(node)[index]
+    let parentNode = document.querySelectorAll(node)[index];
     parentNode.textContent = data[index][attribute];
 
     //Tratando as exceções nas informações
     const fixInfosDetails = () => {
         switch (attribute) {
-            case 'name':
+            case "name":
                 if (data[index][attribute] === "Nidoran ♀ (Female)") {
-                    parentNode.textContent = 'Nidoran ♀';
+                    parentNode.textContent = "Nidoran ♀";
                 }
                 if (data[index][attribute] === "Nidoran ♂ (Male)") {
-                    parentNode.textContent = 'Nidoran ♂';
+                    parentNode.textContent = "Nidoran ♂";
                 }
                 break;
-            case 'img':
+            case "img":
                 parentNode.src = data[index][attribute];
                 break;
-            case 'type':
+            case "type":
                 parentNode.style.width = "150px";
                 if (data[index][attribute].length > 1) {
                     parentNode.textContent = `${data[index][attribute][0]} - ${data[index][attribute][1]}`;
                 }
                 break;
         }
-    }
+    };
     fixInfosDetails();
+};
 
-}
-
-const setCardColor = (index) => document.querySelectorAll('.container-card')[index].style.backgroundColor = colorTypeList[data["pokemon"][index]["type"][0]];
+const setCardColor = (index) =>
+    (document.querySelectorAll(".container-card")[index].style.backgroundColor =
+        colorTypeList[data["pokemon"][index]["type"][0]]);
 
 //Função para carregar cards com as informações dos pokemons
 const loadCards = (dataset) => {
     for (let i = 0; i < dataset.length; i++) {
         cloneCards();
-        setInfosOnCard('.name-pok', i, data["pokemon"], 'name')
-        setInfosOnCard('.number-pok', i, data["pokemon"], 'num')
-        setInfosOnCard('.type-pok', i, data["pokemon"], 'type')
-        setInfosOnCard('.img-pok', i, data["pokemon"], 'img');
+        setInfosOnCard(".name-pok", i, data["pokemon"], "name");
+        setInfosOnCard(".number-pok", i, data["pokemon"], "num");
+        setInfosOnCard(".type-pok", i, data["pokemon"], "type");
+        setInfosOnCard(".img-pok", i, data["pokemon"], "img");
         setCardColor(i);
-        document.querySelectorAll('.container-card')[i].addEventListener("click", () => {
-            clickCard(data["pokemon"][i].name, data["pokemon"][i].num, data["pokemon"][i].type,
-                data["pokemon"][i].img, data["pokemon"][i].height, data["pokemon"][i].weight, data["pokemon"][i].candy,
-                data["pokemon"][i].candy_count, data["pokemon"][i].egg, data["pokemon"][i].spawn_chance)
+        document.querySelectorAll(".container-card")[i].addEventListener("click", () => {
+            clickCard(
+                data["pokemon"][i].name,
+                data["pokemon"][i].num,
+                data["pokemon"][i].type,
+                data["pokemon"][i].img,
+                data["pokemon"][i].height,
+                data["pokemon"][i].weight,
+                data["pokemon"][i].candy,
+                data["pokemon"][i].candy_count,
+                data["pokemon"][i].egg,
+                data["pokemon"][i].spawn_chance
+            );
         });
     }
-    document.querySelector('.container-deck').removeChild(document.querySelectorAll('.container-card')[dataset.length]);
-}
+    document
+        .querySelector(".container-deck")
+        .removeChild(document.querySelectorAll(".container-card")[dataset.length]);
+};
 loadCards(data["pokemon"]);
 
 //Configurações do modal (card estendido)
 function clickCard(name, num, type, img, height, weight, candy, candy_count, egg, spawn_chance) {
-    // const changingInfo = () => {
-    //     if (name === "Nidoran ♀ (Female)") {
-    //         name = "Nidoran ♀";    
-    //     }  else  if (name === "Nidoran ♂ (Male)") {
-    //         name = "Nidoran ♂";
-    //     } else if (type === "type[0],type[1]"){
-    //         type = `${type[0]} - ${type[1]}`;
-    //     } else (candy_count === ""){
-    //         candy_count = 0;
-    //     }
-    // }
-    // changingInfo();
-    document.getElementById("char-name").textContent = name
-    document.getElementById("char-num").textContent = num
-    document.getElementById("char-type").textContent = type
-    document.getElementById("char-img").src = img
-    document.getElementById("char-height-value").textContent = height
-    document.getElementById("char-weight-value").textContent = weight
-    document.getElementById("char-cand-value").textContent = candy
-    document.getElementById("char-cand-count-value").textContent = candy_count
-    document.getElementById("Char-egg-value").textContent = egg
-    document.getElementById("char-spawn-chance-value").textContent = spawn_chance
-        //Abrir modal
+    const changingInfo = () => {
+        switch (name) {
+            case "Nidoran ♀ (Female)":
+                name = "Nidoran ♀";
+                break;
+            case "Nidoran ♂ (Male)":
+                name = "Nidoran ♂";
+                break;
+        }
+
+        // switch (type) {
+        //   case type.length>1:
+        //   type = type.join(" - ");
+        //   console.log(type)
+        //   break;
+        // }
+
+        if (type.length > 1) {
+            type = `${type[0]} - ${type[1]}`
+        }
+
+        switch (candy) {
+            case "Nidoran ♀ (Female) Candy":
+                candy = "Nidoran ♀ Candy";
+                break;
+            case "Nidoran ♂ (Male) Candy":
+                candy = "Nidoran ♂ Candy";
+                break;
+        }
+        // ) if (name === "Nidoran ♀ (Female)"{
+        //   name = "Nidoran ♀";
+        // } else if (name === "Nidoran ♂ (Male)") {
+        //   name = "Nidoran ♂";
+        // } 
+        // if ((type === "type[0]", "type[1]")) {
+        //   type = type.join(" - ");
+        // }
+        // if (candy === "Nidoran ♀ (Female) Candy") {
+        //   candy = "Nidoran ♀ Candy";
+        // } else if (candy === "Nidoran ♂ (Male) Candy") {
+        //   candy = "Nidoran ♂ Candy";
+        // }
+    };
+    changingInfo();
+    document.getElementById("char-name").textContent = name;
+    document.getElementById("char-num").textContent = num;
+    document.getElementById("char-type").textContent = type;
+    document.getElementById("char-img").src = img;
+    document.getElementById("char-height-value").textContent = height;
+    document.getElementById("char-weight-value").textContent = weight;
+    document.getElementById("char-cand-value").textContent = candy;
+    document.getElementById("char-cand-count-value").textContent = candy_count;
+    document.getElementById("Char-egg-value").textContent = egg;
+    document.getElementById("char-spawn-chance-value").textContent = spawn_chance;
+    //Abrir modal
     openModal();
 }
 
-const modal = document.querySelector('.modal-char');
+const modal = document.querySelector(".modal-char");
 
 function openModal() {
-    modal.style.display = "block"
-};
+    modal.style.display = "block";
+}
 
-const closeModal = document.querySelector('.close').addEventListener("click", () => {
-    modal.style.display = "none"
+document.querySelector(".close").addEventListener("click", () => {
+    modal.style.display = "none";
 });
 
 window.addEventListener("click", (event) => {
-    if (event.target == modal) {
-        modal.style.display = "none"
+    if (event.target === modal) {
+        modal.style.display = "none";
     }
 });
 
-
-
-// ----------- Filtragem, Ordenação e Busca de dados -------------//
-
-//filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
+// ----------- Filtragem e Ordenação de dados -------------//
 
 //Recuperação da escolha dos usuários
 function getUserOption(SelectIndex) {
-    const select = document.getElementsByClassName('select')[SelectIndex];
+    const select = document.getElementsByClassName("select")[SelectIndex];
     const optionValue = select.options[select.selectedIndex].value;
     return optionValue;
-};
-
+}
 
 //Filtro:
-const createfilterType = (pokemon) => {
-    //Objetivo: esconder pokemons não selecionados
-    let optionUser = getUserOption(0);
-    if (pokemon.type[0] !== optionUser && pokemon.type[1] !== optionUser) {
-        return pokemon;
-    }
-};
-
-const applyFilterTypeOnCards = (data) => {
-    //Recuperar exibição dos cards ocultos em filtros anteriores
-    let cardList = document.querySelectorAll('.container-card');
-    cardList.forEach((card) => card.style.display = "block");
-
-    //Lista de cards não selecionados que serão ocultos
-    let dataFiltered = data.filter(createfilterType);
-    console.log(dataFiltered);
-
-    //Lista de elementos html que contém o núm dos pokemons
-    let numberNodeList = document.querySelectorAll('.number-pok');
-    //Para cada polemon não selecionado:
-    for (let item of dataFiltered) {
-        //Pegue o número deste pokemon
+const filterType = () => {
+    const condition = getUserOption(0);
+    let cardList = document.querySelectorAll(".container-card");
+    cardList.forEach((card) => (card.style.display = "block"));
+    const pokemonFiltered = filterData(data["pokemon"], condition);
+    let numberNodeList = document.querySelectorAll(".number-pok");
+    for (let item of pokemonFiltered) {
         let pokemonNotFilteredNumber = item.num;
-        //Para cada elemento html com o n° dos pokemons:
         for (let item of numberNodeList) {
-            //Verifique se o número do pokemon não selecionado é igual ao número que está inscrito dentro desse elemento:
             if (pokemonNotFilteredNumber === item.textContent) {
-                //se sim, apague o seu nó avô (.container-card)
-                item.parentNode.parentNode.style.display = "none"
-            };
+                item.parentNode.parentNode.style.display = "none";
+            }
         }
-    };
+    }
 };
 
 
 //Ordenação:
-function sortData(orderBy) {
-    //Recuperar exibição dos cards ocultos em filtros anteriores
-    let cardList = document.querySelectorAll('.container-card');
-    cardList.forEach((card) => card.style.display = "block");
-
-    //recuperar escolha do usuário sobre que tipo de ordem
-    const optionOrderUser = getUserOption(1);
-    //Declaração da variável a ser manipulada dentro da função, dependendo da esolha do usuário
-    let ordenado
-    if (optionOrderUser === "Menor-nº" || optionOrderUser === "A-Z") {
-        ordenado = data["pokemon"].sort((a, b) => a[orderBy] > b[orderBy] ? 1 : -1)
-    }
-    if (optionOrderUser === "Maior-nº" || optionOrderUser === "Z-A") {
-        ordenado = data["pokemon"].sort((a, b) => a[orderBy] > b[orderBy] ? -1 : 1)
-    }
-    console.log(ordenado);
-    loadCards(ordenado)
-}
-
-
-//Stats
-const computeStats = (data) => {
-    console.log(`Minha % é de ${data}`);
-    console.log(data)
-}
-
-//chamada Stats
-computeStats(data)
+const orderData = (sortBy) => {
+    const sortOrder = getUserOption(1);
+    const pokemonOrded = sortData(data["pokemon"], sortBy, sortOrder);
+    loadCards(pokemonOrded);
+};
 
 //Voltar para home page
-const goHomePage = () => window.location.reload()
-const goLaboratoriaPage = () => window.location.href = "https://www.laboratoria.la/"
+const goHomePage = () => window.location.reload();
+const goLaboratoriaPage = () =>
+    (window.location.href = "https://www.laboratoria.la/");
 
 //Atribuição de eventos
-document.querySelector('#home').addEventListener('click', goHomePage);
-document.querySelector('#logo-lab').addEventListener('click', goLaboratoriaPage)
-document.getElementsByClassName('select')[0].addEventListener("change", () => {
-    getUserOption(0);
-    applyFilterTypeOnCards(data.pokemon);
-});
-document.getElementsByClassName('select')[1].addEventListener("change", () => {
+document.querySelector("#home").addEventListener("click", goHomePage);
+document
+    .querySelector("#logo-lab")
+    .addEventListener("click", goLaboratoriaPage);
 
+const optionTyperUser = document.getElementsByClassName("select")[0];
+optionTyperUser.addEventListener("change", () => {
+    getUserOption(0);
+    filterType();
+});
+
+const optionOrderUser = document.getElementsByClassName("select")[1];
+optionOrderUser.addEventListener("change", () => {
     if (getUserOption(1) === "Menor-nº" || getUserOption(1) === "Maior-nº") {
-        sortData("id")
+        orderData("num");
     } else if (getUserOption(1) === "A-Z" || getUserOption(1) === "Z-A") {
-        sortData("name")
+        orderData("name");
     }
 });
